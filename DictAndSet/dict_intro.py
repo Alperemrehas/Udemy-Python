4646fdcfa74c699aae4274cf0231e2e86ea140dc
Alper_Emre <37380232+Alperemrehas@users.noreply.github.com>
2023-07-12 16:15:45 +0300

    Comments are added

--- DictAndSet/dict_intro.py
+++ DictAndSet/dict_intro.py
@@ -1,3 +1,4 @@
+# Define a dictionary of vehicles
 vehicles = {
     'dream': 'Honda 250T',
     'roadster': 'BMW R1100',
@@ -9,17 +10,22 @@ vehicles = {
     'fiesta': 'Ford Fiesta Ghia 1.4',
 }
 
+# Access and assign a specific vehicle to the variable 'my_car'
 my_car = vehicles['fiesta']
-print(my_car)
+print(my_car)  # Print the value of 'my_car'
 
+# Access and assign another specific vehicle to the variable 'commuter'
 commuter = vehicles['virago']
-print(commuter)
+print(commuter)  # Print the value of 'commuter'
 
+# Access a vehicle using the 'get' method and assign it to the variable 'learner'
 learner = vehicles.get("er5")
-print(learner)
+print(learner)  # Print the value of 'learner'
 
+# Iterate over the keys in the 'vehicles' dictionary and print each key-value pair
 for key in vehicles:
     print(key, vehicles[key], sep=", ")
 
+# Iterate over the key-value pairs in the 'vehicles' dictionary and print each pair
 for key, value in vehicles.items():
     print(key, value, sep=", ")
